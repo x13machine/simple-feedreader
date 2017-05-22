f8c548799939190478c0a03e13ee01a814864caf
x13machine <brandanw2@gmail.com>
2017-05-22 16:18:49 -0500

    code

---
+++ index.js
@@ -0,0 +1,41 @@
+var FeedParser = require('feedparser');
+var request = require('request'); // for fetching the feed 
+
+function feed(url,callback){
+	var req = request(url);
+	var feedparser = new FeedParser();
+	 
+	req.on('error', function (err) {
+		callback(err);
+	});
+	 
+	req.on('response', function (res) {
+		if(res.statusCode !== 200) {
+			callback('Bad status code');
+			return ;
+		}
+		this.pipe(feedparser);
+	});
+	 
+	feedparser.on('error', function (error) {
+		callback(err);
+	});
+	
+	var items = []; 
+	feedparser.on('readable', function () {
+		var meta = this.meta;
+		while (item = this.read()) {
+			items.push(item);
+		}
+
+	});
+	
+	feedparser.on('end', function(){
+		callback(null,items);
+	});
+}
+
+
+feed('http://feeds.reuters.com/reuters/businessNews',function(err,res){
+	console.log('done',res[0]);
+});
